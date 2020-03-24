2f5b0b4b1574829836cb213ce9dcd6a7d4f3eba3
jhwann <jhwan@kaist.ac.kr>
2020-03-24 10:51:25 +0900

    Add files via upload

---
+++ cgcnn/data.py
@@ -0,0 +1,165 @@
+from __future__ import print_function, division
+import os
+import csv
+import re
+import json
+import functools
+import random
+
+from ase.io import read
+
+import torch
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.dataloader import default_collate
+from torch.utils.data.sampler import SubsetRandomSampler
+from pymatgen.core.structure import Structure
+from pymatgen.io.ase import *
+
+def get_train_val_test_loader(dataset, collate_fn=default_collate,
+                              batch_size=64, train_idx=None, val_idx=None, test_idx=None,
+                              num_workers=1, pin_memory=False, return_test=False):
+
+	total_size = len(dataset)
+	train_sampler = SubsetRandomSampler(train_idx)
+	val_sampler = SubsetRandomSampler(val_idx)
+	if return_test:
+		test_sampler = SubsetRandomSampler(test_idx)
+
+	train_loader = DataLoader(dataset, batch_size=batch_size,
+                            sampler=train_sampler,
+                            num_workers=num_workers,
+                            collate_fn=collate_fn, pin_memory=pin_memory)
+
+	val_loader = DataLoader(dataset, batch_size=batch_size,
+                          sampler=val_sampler,
+                          num_workers=num_workers,
+                          collate_fn=collate_fn, pin_memory=pin_memory)
+	if return_test:
+		test_loader = DataLoader(dataset, batch_size=batch_size,
+                             sampler=test_sampler,
+                             num_workers=num_workers,
+                             collate_fn=collate_fn, pin_memory=pin_memory)
+
+	if return_test:
+		return train_loader, val_loader, test_loader
+	else:
+		return train_loader, val_loader
+
+def collate_pool(dataset_list):
+    batch_atom_fea, batch_nbr_fea, batch_nbr_fea_idx = [], [], []
+    crystal_atom_idx, batch_target = [], []
+    batch_cif_ids = []
+    base_idx = 0
+    for i, ((atom_fea, nbr_fea, nbr_fea_idx), target, cif_id)\
+            in enumerate(dataset_list):
+        n_i = atom_fea.shape[0]  # number of atoms for this crystal
+        batch_atom_fea.append(atom_fea)
+        batch_nbr_fea.append(nbr_fea)
+        batch_nbr_fea_idx.append(nbr_fea_idx+base_idx)
+        new_idx = torch.LongTensor(np.arange(n_i)+base_idx)
+        crystal_atom_idx.append(new_idx)
+        batch_target.append(target)
+        batch_cif_ids.append(cif_id)
+        base_idx += n_i
+    return (torch.cat(batch_atom_fea, dim=0),
+            torch.cat(batch_nbr_fea, dim=0),
+            torch.cat(batch_nbr_fea_idx, dim=0),
+            crystal_atom_idx),\
+        torch.stack(batch_target, dim=0),\
+        batch_cif_ids
+
+
+class GaussianDistance(object):
+    def __init__(self, dmin, dmax, step, var=None):
+        assert dmin < dmax
+        assert dmax - dmin > step
+        self.filter = np.arange(dmin, dmax+step, step)
+        if var is None:
+            var = step
+        self.var = var
+
+    def expand(self, distances):
+        return np.exp(-(distances[..., np.newaxis] - self.filter)**2 /
+                      self.var**2)
+
+
+class AtomInitializer(object):
+    def __init__(self, atom_types):
+        self.atom_types = set(atom_types)
+        self._embedding = {}
+
+    def get_atom_fea(self, atom_type):
+        assert atom_type in self.atom_types
+        return self._embedding[atom_type]
+
+    def load_state_dict(self, state_dict):
+        self._embedding = state_dict
+        self.atom_types = set(self._embedding.keys())
+        self._decodedict = {idx: atom_type for atom_type, idx in
+                            self._embedding.items()}
+
+    def state_dict(self):
+        return self._embedding
+
+    def decode(self, idx):
+        if not hasattr(self, '_decodedict'):
+            self._decodedict = {idx: atom_type for atom_type, idx in
+                                self._embedding.items()}
+        return self._decodedict[idx]
+
+
+class AtomCustomJSONInitializer(AtomInitializer):
+    def __init__(self, elem_embedding_file):
+        with open(elem_embedding_file) as f:
+            elem_embedding = json.load(f)
+        elem_embedding = {int(key): value for key, value
+                          in elem_embedding.items()}
+        atom_types = set(elem_embedding.keys())
+        super(AtomCustomJSONInitializer, self).__init__(atom_types)
+        for key, value in elem_embedding.items():
+            self._embedding[key] = np.array(value, dtype=float)
+
+
+class CIFData(Dataset):
+    def __init__(self, root_dir, max_num_nbr=9, radius=12, dmin=0, step=0.2,
+                 random_seed=123):
+        self.root_dir = root_dir
+        self.max_num_nbr, self.radius = max_num_nbr, radius
+        assert os.path.exists(root_dir), 'root_dir does not exist!'
+        id_prop_file = os.path.join(self.root_dir, 'id_prop.csv')
+        assert os.path.exists(id_prop_file), 'id_prop.csv does not exist!'
+        with open(id_prop_file) as f:
+            reader = csv.reader(f)
+            self.id_prop_data = [row for row in reader]
+        np.random.seed(random_seed)
+        np.random.shuffle(self.id_prop_data)
+        atom_init_file = os.path.join(self.root_dir, 'atom_init.json')
+        assert os.path.exists(atom_init_file), 'atom_init.json does not exist!'
+        self.ari = AtomCustomJSONInitializer(atom_init_file)
+        self.gdf = GaussianDistance(dmin=dmin, dmax=self.radius, step=step)
+
+    def __len__(self):
+        return len(self.id_prop_data)
+
+    @functools.lru_cache(maxsize=None)  # Cache loaded structures
+    def __getitem__(self, idx):
+        cif_id, target = self.id_prop_data[idx]
+        atoms = read(os.path.join(self.root_dir,cif_id+'.vasp'))
+        #atoms = read(os.path.join(self.root_dir,cif_id+'.cif'))
+        crystal = AseAtomsAdaptor.get_structure(atoms)
+        atom_fea = np.vstack([self.ari.get_atom_fea(crystal[i].specie.number)
+                              for i in range(len(crystal))])
+        atom_fea = torch.Tensor(atom_fea)
+        all_nbrs = crystal.get_all_neighbors(self.radius, include_index=True)
+        all_nbrs = [sorted(nbrs, key=lambda x: x[1]) for nbrs in all_nbrs] # sorting w.r.t. distance
+        nbr_fea_idx = np.array([list(map(lambda x: x[2],
+                                nbr[:self.max_num_nbr])) for nbr in all_nbrs])
+        nbr_fea = np.array([list(map(lambda x: x[1], nbr[:self.max_num_nbr]))
+                            for nbr in all_nbrs])
+        nbr_fea = self.gdf.expand(nbr_fea)
+        atom_fea = torch.Tensor(atom_fea)
+        nbr_fea = torch.Tensor(nbr_fea)
+        nbr_fea_idx = torch.LongTensor(nbr_fea_idx)
+        target = torch.Tensor([float(target)])
+        return (atom_fea, nbr_fea, nbr_fea_idx), target, cif_id
